eec75b9b647a46e33cc78b8aa6e6d485930f3a4a
raj8789 <raushanraj8789@gmail.com>
2025-02-13 14:47:58 +0530

    Alert Dialog Has been added in code

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -23,11 +24,19 @@ class MainActivity : AppCompatActivity() {
         }
         var button=findViewById<Button>(R.id.screen1button)
         button.setOnClickListener(){
-            val intent: Intent = Intent(
-                this,
-                    Activity2::class.java
-            )
-            startActivity(intent)
+//            val intent: Intent = Intent(
+//                this,
+//                    Activity2::class.java
+//            )
+//            startActivity(intent)
+
+
+           var builder= AlertDialog.Builder(this)
+            builder.setMessage("Alert On the Button")
+            builder.setTitle("Alert Dialog")
+            builder.setCancelable(true)
+            builder.setNegativeButton("Close"){_,_->}
+            builder.create().show()
         }
     }
 }
